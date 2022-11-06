c96c7d6a519334b823eed07ad128324d429fc753
Anton Khrapov <froe054@mail.ru>
2022-11-06 11:53:51 +0700

    initial commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '15.2'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'HW21' do
+  pod 'Alamofire', '~> 5.5'
+end
---
+++ Pods/Target Support Files/Pods-HW21/Pods-HW21-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HW21 : NSObject
+@end
+@implementation PodsDummy_Pods_HW21
+@end
